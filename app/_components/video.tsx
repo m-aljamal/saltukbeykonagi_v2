03cabbf0730c566c2665d16c6acc5cb557fac213
m-jamal <mohammadjamol@gmail.com>
2024-06-11 16:26:32 +0300

    finish

---
+++ app/_components/video.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React, { useRef } from "react";
+
+interface HoverVideoProps {
+  src: string;
+  type: string;
+  
+  
+  muted?: boolean;
+}
+
+export default function Video({
+  src,
+  type,
+  
+  muted,
+  
+}: HoverVideoProps) {
+  return (
+    <video className="h-[400px]"  muted={muted} controls>
+      <source src={src} type={type} />
+      Your browser does not support the video tag.
+    </video>
+  );
+}
--- app/neden-kilis/page.tsx
+++ app/neden-kilis/page.tsx
@@ -1,154 +1,148 @@
 import { CheckCircle, Info } from "lucide-react";
 import PageSheet from "../_components/page-sheet";
+import Video from "../_components/video";
+import Image from "next/image";
+
+const data = [
+  {
+    title: "Tarihi ve Kültürel Zenginlik",
+    description:
+      "Kilis, pek çok medeniyete ev sahipliği yapmış köklü bir geçmişe sahiptir. Şehirdeki tarihi camiler, kiliseler, hanlar ve hamamlar ziyaretçilerine tarih boyunca farklı kültürlerin izlerini görme fırsatı sunar.",
+    images: [
+      "/images/d6.jpeg",
+      "/images/d7.jpeg",
+      "/images/d8.jpeg",
+      "/images/d10.jpeg",
+    ],
+    videos: [],
+  },
+  {
+    title: "Gastronomi",
+    description:
+      "Kilis, zengin mutfağıyla da ünlüdür. Özellikle Kilis tava, oruk (içli köfte), cennet çamuru (tatlısı) gibi yerel lezzetler tadılmaya değerdir. Ayrıca Kilis zeytinyağı ve üzüm ürünleri de oldukça meşhurdur.",
+    images: [],
+    videos: ["/videos/v1.mp4", "/videos/v2.mp4", "/videos/v4.mp4"],
+  },
+  {
+    title: "Doğal Güzellikler",
+    description:
+      "Kilis’in doğal güzellikleri ve yeşil alanları, doğa severler için çekici bir destinasyon haline gelmektedir. Şehrin çevresindeki bağlar ve zeytinlikler huzur verici bir atmosfer sunar.",
+    images: ["/images/d5.jpeg", "/images/d9.jpeg"],
+    videos: [],
+  },
+  {
+    title: "Misafirperverlik",
+    description:
+      "Kilis halkı, misafirperverliği ve sıcak kanlılığıyla bilinir. Burada samimi ve dostça karşılanırsınız, yerel halkın kültürünü ve yaşam tarzını yakından tanıma fırsatınız olur.",
+    images: [],
+    videos: ["/videos/v3.mp4"],
+  },
+  {
+    title: "El Sanatları ve Hediyelik Eşyalar",
+    description:
+      "Kilis'te geleneksel el sanatları oldukça gelişmiştir. El yapımı halılar, kilimler, bakır eşyalar ve diğer el sanatı ürünleri, hem hediyelik olarak hem de ev dekorasyonu için tercih edilebilir.",
+    images: ["/images/d12.jpeg"],
+    videos: [],
+  },
+  {
+    title: "Etkinlikler ve Festivaller",
+    description:
+      "Kilis, çeşitli kültürel etkinlikler ve festivallere de ev sahipliği yapmaktadır. Özellikle üzüm ve zeytin festivalleri, yerel kültürü yakından tanıma fırsatı sunar.",
+    images: ["/images/d4.jpeg", "/images/d11.jpeg"],
+    videos: [],
+  },
+  {
+    title: "Sakin ve Huzurlu Atmosfer",
+    description:
+      "Kilis, büyük şehirlerin kalabalık ve gürültüsünden uzak, daha sakin ve huzurlu bir ortam sunar. Tarihi sokaklarda yürüyüş yaparken veya bir kafede oturup çay içerken huzuru hissedebilirsiniz.",
+    images: [],
+    videos: [],
+  },
+];
 
 export default function page() {
   return (
     <PageSheet>
-      <div className="   ">
-        <div className="mx-auto max-w-3xl text-base leading-7 text-gray-700">
-          <p className="text-base font-semibold leading-7 text-indigo-600">
-            Introducing
-          </p>
-          <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            JavaScript for Beginners
-          </h1>
-          <p className="mt-6 text-xl leading-8">
-            Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At
-            arcu, sit dui mi, nibh dui, diam eget aliquam. Quisque id at vitae
-            feugiat egestas ac. Diam nulla orci at in viverra scelerisque eget.
-            Eleifend egestas fringilla sapien.
-          </p>
-          <div className="mt-10 max-w-2xl">
-            <p>
-              Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
-              enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
-              praesent donec est. Odio penatibus risus viverra tellus varius sit
-              neque erat velit. Faucibus commodo massa rhoncus, volutpat.
-              Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae
-              sed turpis id.
-            </p>
-            <ul role="list" className="mt-8 max-w-xl space-y-8 text-gray-600">
-              <li className="flex gap-x-3">
-                <CheckCircle
-                  className="mt-1 h-5 w-5 flex-none text-indigo-600"
-                  aria-hidden="true"
-                />
-                <span>
-                  <strong className="font-semibold text-gray-900">
-                    Data types.
-                  </strong>{" "}
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                  Maiores impedit perferendis suscipit eaque, iste dolor
-                  cupiditate blanditiis ratione.
-                </span>
-              </li>
-              <li className="flex gap-x-3">
-                <CheckCircle
-                  className="mt-1 h-5 w-5 flex-none text-indigo-600"
-                  aria-hidden="true"
-                />
-                <span>
-                  <strong className="font-semibold text-gray-900">
-                    Loops.
-                  </strong>{" "}
-                  Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
-                  qui lorem cupidatat commodo.
-                </span>
-              </li>
-              <li className="flex gap-x-3">
-                <CheckCircle
-                  className="mt-1 h-5 w-5 flex-none text-indigo-600"
-                  aria-hidden="true"
-                />
-                <span>
-                  <strong className="font-semibold text-gray-900">
-                    Events.
-                  </strong>{" "}
-                  Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus.
-                  Et magna sit morbi lobortis.
-                </span>
-              </li>
-            </ul>
-            <p className="mt-8">
-              Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis
-              odio id et. Id blandit molestie auctor fermentum dignissim. Lacus
-              diam tincidunt ac cursus in vel. Mauris varius vulputate et
-              ultrices hac adipiscing egestas. Iaculis convallis ac tempor et
-              ut. Ac lorem vel integer orci.
-            </p>
-            <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">
-              From beginner to expert in 3 hours
+      <div>
+        <div className="mx-auto max-w-5xl text-base leading-7 text-gray-700">
+          <div className="mx-auto max-w-xl text-center">
+            <h2 className="text-lg font-semibold leading-8 tracking-tight text-yellow-900">
+              Neden Kilis ?
             </h2>
-            <p className="mt-6">
-              Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam
-              consequat in. Convallis arcu ipsum urna nibh. Pharetra, euismod
-              vitae interdum mauris enim, consequat vulputate nibh. Maecenas
-              pellentesque id sed tellus mauris, ultrices mauris. Tincidunt enim
-              cursus ridiculus mi. Pellentesque nam sed nullam sed diam turpis
-              ipsum eu a sed convallis diam.
-            </p>
-            <figure className="mt-10 border-l border-indigo-600 pl-9">
-              <blockquote className="font-semibold text-gray-900">
-                <p>
-                  “Vel ultricies morbi odio facilisi ultrices accumsan donec
-                  lacus purus. Lectus nibh ullamcorper ac dictum justo in
-                  euismod. Risus aenean ut elit massa. In amet aliquet eget
-                  cras. Sem volutpat enim tristique.”
-                </p>
-              </blockquote>
-              <figcaption className="mt-6 flex gap-x-4">
-                <img
-                  className="h-6 w-6 flex-none rounded-full bg-gray-50"
-                  src="https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  alt=""
-                />
-                <div className="text-sm leading-6">
-                  <strong className="font-semibold text-gray-900">
-                    Maria Hill
-                  </strong>{" "}
-                  – Marketing Manager
-                </div>
-              </figcaption>
-            </figure>
-            <p className="mt-10">
-              Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
-              enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
-              praesent donec est. Odio penatibus risus viverra tellus varius sit
-              neque erat velit.
+            <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+              Saltuk Bey Konağı Otel
             </p>
           </div>
-          <figure className="mt-16">
-            <img
-              className="aspect-video rounded-xl bg-gray-50 object-cover"
-              src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&auto=format&fit=facearea&w=1310&h=873&q=80&facepad=3"
-              alt=""
-            />
-            <figcaption className="mt-4 flex gap-x-2 text-sm leading-6 text-gray-500">
-              <Info
-                className="mt-0.5 h-5 w-5 flex-none text-gray-300"
-                aria-hidden="true"
+
+          <p className="mt-6 text-xl leading-8">
+           {` Kilis, Türkiye'nin güneydoğusunda yer alan tarihi ve kültürel
+            zenginlikleriyle öne çıkan bir şehirdir. Kilis'e gitmek için birçok
+            sebep bulunmaktadır:`}
+          </p>
+          <div className="mt-10  ">
+            <ul role="list" className="mt-8  space-y-8 text-gray-600">
+              {data.map((item) => (
+                <li key={item.title} className="space-y-5">
+                  <div className="flex gap-x-3 max-w-xl">
+                    <CheckCircle
+                      className="mt-1 h-5 w-5 flex-none text-yellow-600"
+                      aria-hidden="true"
+                    />
+                    <span>
+                      <strong className="font-semibold text-gray-900">
+                        {item.title}
+                      </strong>{" "}
+                      {item.description}
+                    </span>
+                  </div>
+                  {item.images.length > 0 && (
+                    <div className="grid grid-cols-1  md:grid-cols-4 gap-5 ml-8  md:h-[300px] h-[800px]">
+                      {item.images?.map((image) => (
+                        <div className="relative" key={image}>
+                          <Image
+                            src={image}
+                            alt="Image"
+                            className="rounded-md h-full w-full object-cover md:object-fill object-center"
+                            fill
+                          />
+                        </div>
+                      ))}
+                    </div>
+                  )}
+                  {item.videos.length > 0 && (
+                    <div className="ml-8 grid grid-cols-1 md:grid-cols-4 gap-5">
+                      {item.videos?.map((video) => (
+                        <Video
+                          key={video}
+                          src={video}
+                          type="video/mp4"
+                           
+                        />
+                      ))}
+                    </div>
+                  )}
+                </li>
+              ))}
+            </ul>
+
+            <figure className="mt-10 border-l border-yellow-600 pl-9">
+              <blockquote className="font-semibold text-gray-900">
+                <p>
+                  {`Kilis'e yapacağınız bir gezi, hem tarihi ve kültürel
+                  zenginlikleri keşfetmek hem de yöresel lezzetlerin tadına
+                  bakmak için harika bir fırsat olacaktır.`}
+                </p>
+              </blockquote>
+            </figure>
+            <div className="relative mt-10 ">
+              <Image
+                src="/images/d2.jpeg"
+                width={600}
+                height={400}
+                alt="Kilis"
+                className="h-full w-full object-contain object-center"
               />
-              Faucibus commodo massa rhoncus, volutpat.
-            </figcaption>
-          </figure>
-          <div className="mt-16 max-w-2xl">
-            <h2 className="text-2xl font-bold tracking-tight text-gray-900">
-              Everything you need to get up and running
-            </h2>
-            <p className="mt-6">
-              Purus morbi dignissim senectus mattis adipiscing. Amet, massa quam
-              varius orci dapibus volutpat cras. In amet eu ridiculus leo
-              sodales cursus tristique. Tincidunt sed tempus ut viverra
-              ridiculus non molestie. Gravida quis fringilla amet eget dui
-              tempor dignissim. Facilisis auctor venenatis varius nunc, congue
-              erat ac. Cras fermentum convallis quam.
-            </p>
-            <p className="mt-8">
-              Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
-              enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
-              praesent donec est. Odio penatibus risus viverra tellus varius sit
-              neque erat velit.
-            </p>
+            </div>
           </div>
         </div>
       </div>
